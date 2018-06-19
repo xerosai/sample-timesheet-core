23d3b93734b88f3058f88add3b1bc292edbe8318
Simon Neufville <simon@xrscodeworks.com>
2018-06-19 17:46:09 -0500

    initial commit

---
+++ timesheet-core.js
@@ -0,0 +1,41 @@
+/**
+ * @author Simon Neufville <simon@xrscodeworks.com / sneufville@teammps.com>
+ * Created: June 19 2018 @ 17:32
+ * @description Main entry point of the Time sheet API. Routes will be handled by its own router module
+ */
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+// Apply middleware
+app.use(bodyParser.json({
+    limit: '256kb'
+}));
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+if (process.env.NODE_ENV !== 'production') {
+    console.log('Loading development modules');
+    const morgan = require('morgan');
+    app.use(morgan('dev'));
+}
+
+// TODO: Implement Application Router
+app.get('/', (req, res) => {
+    res.json({
+        success: true,
+        message: 'Default route ok'
+    });
+});
+
+// SERVER STARTUP
+const PORT = process.env.SERVER_PORT || 8101;
+
+app.listen(PORT, () => {
+    console.log(`Started API server on port: ${PORT}`);
+    console.log(`API routes have a base URL of: /api/v1`);
+});
